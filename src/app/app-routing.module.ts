f633bc897b9f1a990c0fba80ff3617468777f5e7
AngelRodasHN <81601762+AngelRodasHN@users.noreply.github.com>
2021-05-20 20:22:03 -0600

    Front End Devolpment (Continuation)
    -Change the Log-in page
    -Added the Home page
    -Added a Navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path: 'Home',component : HomeComponent},
-  {path: '', redirectTo: '/Home',pathMatch: 'full'}
+  {path: 'Login',component : LoginComponent},
+  
+  {path: '', redirectTo: '/Login',pathMatch: 'full'}
 
 ];
 
