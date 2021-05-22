e8b8d34c3a02167d3db23c1759bb3b5b95e00ce0
AngelRodasHN <81601762+AngelRodasHN@users.noreply.github.com>
2021-05-22 07:32:57 -0600

    Front-End Development
    -Added the Calculator page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalculatorComponent } from './calculator/calculator.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path: 'Home',component : HomeComponent},
   {path: 'Login',component : LoginComponent},
+  {path: 'Calculator', component : CalculatorComponent},
   
   {path: '', redirectTo: '/Login',pathMatch: 'full'}
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { LoginComponent } from './login/login.component';
     AppComponent,
     HomeComponent,
     LoginComponent,
+    CalculatorComponent,
  
   ],
   imports: [
